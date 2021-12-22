9e76e9c77c32c3c40d91520479e9596c1e9d349c
Sergey Melnychuk <serg.meln@gmail.com>
2021-12-22 22:48:22 +0100

    cargo clippy && cargo fmt

--- src/day22.rs
+++ src/day22.rs
@@ -1,5 +1,5 @@
-use std::collections::HashSet;
 use advent_of_code_2021::util::lines;
+use std::collections::HashSet;
 
 #[derive(Debug, Eq, PartialEq, Copy, Clone, Hash)]
 struct Dot {
@@ -33,16 +33,23 @@ impl Cube {
     }
 
     fn size(&self) -> usize {
-        (self.x1 - self.x0 + 1).abs() as usize *
-        (self.y1 - self.y0 + 1).abs() as usize *
-        (self.z1 - self.z0 + 1).abs() as usize
+        (self.x1 - self.x0 + 1).abs() as usize
+            * (self.y1 - self.y0 + 1).abs() as usize
+            * (self.z1 - self.z0 + 1).abs() as usize
     }
 
     fn and(&self, that: &Cube) -> Option<Cube> {
         let (x0, x1) = overlap(self.x0, self.x1, that.x0, that.x1)?;
         let (y0, y1) = overlap(self.y0, self.y1, that.y0, that.y1)?;
         let (z0, z1) = overlap(self.z0, self.z1, that.z0, that.z1)?;
-        Some(Cube { x0, x1, y0, y1, z0, z1 })
+        Some(Cube {
+            x0,
+            x1,
+            y0,
+            y1,
+            z0,
+            z1,
+        })
     }
 }
 
@@ -53,8 +60,12 @@ fn overlap(a: isize, b: isize, c: isize, d: isize) -> Option<(isize, isize)> {
     let b0 = c.min(d);
     let b1 = c.max(d);
 
-    if a0 < b0 && a1 < b0 { return None; }
-    if a0 > b1 && a1 > b1 { return None; }
+    if a0 < b0 && a1 < b0 {
+        return None;
+    }
+    if a0 > b1 && a1 > b1 {
+        return None;
+    }
 
     Some((a0.max(b0), a1.min(b1)))
 }
@@ -64,9 +75,27 @@ fn parse(s: &str) -> (bool, Cube) {
     let flag_part = it.next().unwrap();
 
     let mut cube_part = it.next().unwrap().split(',');
-    let mut x_range = cube_part.next().unwrap().split('=').skip(1).next().unwrap().split("..");
-    let mut y_range = cube_part.next().unwrap().split('=').skip(1).next().unwrap().split("..");
-    let mut z_range = cube_part.next().unwrap().split('=').skip(1).next().unwrap().split("..");
+    let mut x_range = cube_part
+        .next()
+        .unwrap()
+        .split('=')
+        .nth(1)
+        .unwrap()
+        .split("..");
+    let mut y_range = cube_part
+        .next()
+        .unwrap()
+        .split('=')
+        .nth(1)
+        .unwrap()
+        .split("..");
+    let mut z_range = cube_part
+        .next()
+        .unwrap()
+        .split('=')
+        .nth(1)
+        .unwrap()
+        .split("..");
 
     let cube = Cube {
         x0: x_range.next().unwrap().parse().unwrap(),
@@ -80,17 +109,20 @@ fn parse(s: &str) -> (bool, Cube) {
     match flag_part {
         "on" => (true, cube),
         "off" => (false, cube),
-        _ => unreachable!()
+        _ => unreachable!(),
     }
 }
 
 fn part1(ops: &[(bool, Cube)], init: &Cube) -> usize {
     ops.iter()
-        .flat_map(|(flag, cube)|
-            cube.and(&init).map(|c| c.dots()).unwrap_or_default()
+        .flat_map(|(flag, cube)| {
+            cube.and(init)
+                .map(|c| c.dots())
+                .unwrap_or_default()
                 .into_iter()
                 .map(|d| (flag, d))
-                .collect::<Vec<_>>())
+                .collect::<Vec<_>>()
+        })
         .fold(HashSet::new(), |mut set, (on, dot)| {
             if *on {
                 set.insert(dot);
@@ -113,7 +145,9 @@ fn cut_one(a: isize, b: isize, c: isize, d: isize) -> Vec<(isize, isize)> {
     // |-----------|
     // a           b
     // |-----------|
-    if a == c && b == d { return vec![(a, b)]; }
+    if a == c && b == d {
+        return vec![(a, b)];
+    }
 
     // lo end
     // a            b
@@ -123,10 +157,7 @@ fn cut_one(a: isize, b: isize, c: isize, d: isize) -> Vec<(isize, isize)> {
     // c        d   b
     // |--------||--|
     if a == c && d < b {
-        return vec![
-            (a, d),
-            (d+1, b)
-        ];
+        return vec![(a, d), (d + 1, b)];
     }
 
     // hi end
@@ -137,10 +168,7 @@ fn cut_one(a: isize, b: isize, c: isize, d: isize) -> Vec<(isize, isize)> {
     // a   c        d
     // |--||--------|
     if c > a && d == b {
-        return vec![
-            (a, c-1),
-            (c, d),
-        ];
+        return vec![(a, c - 1), (c, d)];
     }
 
     // middle
@@ -150,11 +178,7 @@ fn cut_one(a: isize, b: isize, c: isize, d: isize) -> Vec<(isize, isize)> {
     // ---|------|----
     // a  c      d   b
     // |-||------||--|
-    vec![
-        (a, c-1),
-        (c, d),
-        (d+1, b),
-    ]
+    vec![(a, c - 1), (c, d), (d + 1, b)]
 }
 
 fn cut(this: &Cube, that: &Cube) -> Vec<Cube> {
@@ -163,7 +187,14 @@ fn cut(this: &Cube, that: &Cube) -> Vec<Cube> {
         for (x0, x1) in cut_one(this.x0, this.x1, that.x0, that.x1) {
             for (y0, y1) in cut_one(this.y0, this.y1, that.y0, that.y1) {
                 for (z0, z1) in cut_one(this.z0, this.z1, that.z0, that.z1) {
-                    let cube = Cube { x0, x1, y0, y1, z0, z1 };
+                    let cube = Cube {
+                        x0,
+                        x1,
+                        y0,
+                        y1,
+                        z0,
+                        z1,
+                    };
                     if cube == that {
                         continue;
                     }
@@ -173,18 +204,16 @@ fn cut(this: &Cube, that: &Cube) -> Vec<Cube> {
         }
         cubes
     } else {
-        vec![this.clone()]
+        vec![*this]
     }
 }
 
 fn part2(ops: &[(bool, Cube)]) -> usize {
     let mut done: Vec<Cube> = Vec::new();
     for (flag, cube) in ops {
-        done = done.into_iter()
-            .flat_map(|x| cut(&x, &cube))
-            .collect();
+        done = done.into_iter().flat_map(|x| cut(&x, cube)).collect();
         if *flag {
-            done.push(cube.clone());
+            done.push(*cube);
         }
     }
 
@@ -192,15 +221,21 @@ fn part2(ops: &[(bool, Cube)]) -> usize {
 }
 
 fn main() {
-    let ops = lines().into_iter()
-        .map(|s| parse(&s))
-        .collect::<Vec<_>>();
+    let ops = lines().into_iter().map(|s| parse(&s)).collect::<Vec<_>>();
 
-    let init = Cube { x0: -50, x1: 50, y0: -50, y1: 50, z0: -50, z1: 50 };
+    let init = Cube {
+        x0: -50,
+        x1: 50,
+        y0: -50,
+        y1: 50,
+        z0: -50,
+        z1: 50,
+    };
     let part1 = part1(&ops, &init);
     println!("{}", part1);
 
-    let part1_ops = ops.iter()
+    let part1_ops = ops
+        .iter()
         .cloned()
         .flat_map(|(flag, cube)| cube.and(&init).map(|cube| (flag, cube)))
         .collect::<Vec<_>>();
@@ -215,27 +250,48 @@ mod tests {
 
     #[test]
     fn test_parse() {
-        assert_eq!(parse("on x=-20..26,y=-36..17,z=-47..7"), (true, (Cube { x0: -20, x1: 26, y0: -36, y1: 17, z0: -47, z1: 7 })));
+        assert_eq!(
+            parse("on x=-20..26,y=-36..17,z=-47..7"),
+            (
+                true,
+                (Cube {
+                    x0: -20,
+                    x1: 26,
+                    y0: -36,
+                    y1: 17,
+                    z0: -47,
+                    z1: 7
+                })
+            )
+        );
     }
 
     #[test]
     fn test_size() {
-        assert_eq!(Cube {
-            x0: 0,
-            x1: 0,
-            y0: 0,
-            y1: 0,
-            z0: 0,
-            z1: 0,
-        }.size(), 1);
+        assert_eq!(
+            Cube {
+                x0: 0,
+                x1: 0,
+                y0: 0,
+                y1: 0,
+                z0: 0,
+                z1: 0,
+            }
+            .size(),
+            1
+        );
 
-        assert_eq!(Cube {
-            x0: 0,
-            x1: 6,
-            y0: 0,
-            y1: 4,
-            z0: 0,
-            z1: 0,
-        }.size(), 35);
+        assert_eq!(
+            Cube {
+                x0: 0,
+                x1: 6,
+                y0: 0,
+                y1: 4,
+                z0: 0,
+                z1: 0,
+            }
+            .size(),
+            35
+        );
     }
 }
